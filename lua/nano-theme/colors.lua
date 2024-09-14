048a1647d2eb2599614339126ed91e13da5cd496
Diego Montoya Castro <markii@mac.local>
2024-09-14 09:52:26 -0600

    teset

--- lua/nano-theme/colors.lua
+++ lua/nano-theme/colors.lua
@@ -27,7 +27,7 @@ function M.get()
   local nano_veryfaded_color  = is_light_theme and "#BBCCCE" or "#506071"
   local nano_salient_color    = is_light_theme and "#325CC0" or "#81A1C1"
   --local nano_strong_color     = is_light_theme and "#263238" or "#FFFFFF"
-  local nano_strong_color     = is_light_theme and "#006400" or "#FFFFFF"
+  local nano_strong_color     = is_light_theme and "#325CC9" or "#FFFFFF"
   local nano_popout_color     = is_light_theme and "#EECC20" or "#FFAB91"
   --local nano_popout_color     = is_light_theme and "#FFAB91" or "#D08770"
   local nano_critical_color   = is_light_theme and "#FF6F00" or "#EBCB8B"
@@ -73,7 +73,7 @@ function M.get()
     nano_strong     = { fg = nano_strong_color, bold = false },
     nano_popout     = { fg = nano_popout_color },
     nano_critical   = { fg = nano_critical_color },
-    nano_string     = { fg = yellow },
+    nano_string     = { fg = green },
     nano_const      = { fg = const_color },
     nano_comment    = { fg = comment_color },
 
