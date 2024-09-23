b2b623364d98d094be3135abb88e24744a5cbcdd
Diego <dalonso77@gmail.com>
2024-09-23 11:55:46 -0600

    Test

--- lua/nano-theme/colors.lua
+++ lua/nano-theme/colors.lua
@@ -19,7 +19,7 @@ function M.get()
   local  base6 = is_light_theme and "#474747" or "#9099AB"
   local  base7 = is_light_theme and "#282828" or "#D8DEE9"
 
-  local nano_foreground_color = is_light_theme and "#000000" or "#ECEFF4"
+  local nano_foreground_color = is_light_theme and "#212529" or "#ECEFF4"
   --local nano_foreground_color = is_light_theme and "#37474F" or "#ECEFF4"
   local nano_background_color = is_light_theme and "#F7F7F7" or "#2E3440"
   local nano_highlight_color  = is_light_theme and "#E8E8E8" or "#3B4252"
