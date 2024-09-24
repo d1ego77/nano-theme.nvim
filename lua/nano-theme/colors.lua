ce5495c2c9615a8ce96defc752d7b1facf542191
Diego <dalonso77@gmail.com>
2024-09-24 10:14:05 -0600

    background change

--- lua/nano-theme/colors.lua
+++ lua/nano-theme/colors.lua
@@ -23,7 +23,7 @@ function M.get()
   --local nano_foreground_color = is_light_theme and "#37474F" or "#ECEFF4"
   local nano_background_color = is_light_theme and "#F3F3F3" or "#2E3440"
   local nano_highlight_color  = is_light_theme and "#D8DEE9" or "#3B4252"
-  local nano_subtle_color     = is_light_theme and "#FFCDFF" or "#434C5E"
+  local nano_subtle_color     = is_light_theme and "#FFEFE3" or "#434C5E"
   local nano_faded_color      = is_light_theme and "#90A4AE" or "#677691"
   local nano_veryfaded_color  = is_light_theme and "#BBCCCE" or "#506071"
   local nano_salient_color    = is_light_theme and "#325CC0" or "#81A1C1"
