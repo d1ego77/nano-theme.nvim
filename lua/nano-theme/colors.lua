059902e2021befea385277acd514885843b601bc
Diego <dalonso77@gmail.com>
2024-09-26 14:06:48 -0600

    selection change

--- lua/nano-theme/colors.lua
+++ lua/nano-theme/colors.lua
@@ -42,7 +42,7 @@ function M.get()
   local yellow  = "#F1C21B"
 
   local const_color = "#6929C4"
-  local comment_color = "#ff8389"
+  local comment_color = "#da1e28"
   local colors = {
     -- Colors ------------------------------------------------------------------------------
 
